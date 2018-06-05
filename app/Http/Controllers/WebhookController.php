a783c96df3052112d7cead17c856f39b2baba319
Ramdhan <ramdhanrizki11@gmail.com>
2018-06-05 12:32:26 +0700

    Initial Commit

---
+++ app/Http/Controllers/WebhookController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use LINE\LINEBot;
+use LINE\LINEBot\Constant\HTTPHeader;
+use LINE\LINEBot\HTTPClient\CurlHTTPClient;
+use LINE\LINEBot\Event\MessageEvent;
+use LINE\LINEBot\Event\MessageEvent\TextMessage;
+use LINE\LINEBot\Event\PostbackEvent;
+use LINE\LINEBot\MessageBuilder\TextMessageBuilder;
+use LINE\LINEBot\MessageBuilder\TemplateMessageBuilder;
+use LINE\LINEBot\MessageBuilder\TemplateBuilder\ButtonTemplateBuilder;
+use LINE\LINEBot\MessageBuilder\TemplateBuilder\ConfirmTemplateBuilder;
+use LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselColumnTemplateBuilder;
+use LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselTemplateBuilder;
+use LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder;
+use LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder;
+use LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder;
+use LINE\LINEBot\ImagemapActionBuilder\ImagemapMessageActionBuilder;
+use LINE\LINEBot\ImagemapActionBuilder\AreaBuilder;
+use LINE\LINEBot\MessageBuilder\ImagemapMessageBuilder;
+use LINE\LINEBot\MessageBuilder\Imagemap\BaseSizeBuilder;
+use Illuminate\Foundation\Inspiring;
+use LINE\LINEBot\MessageBuilder\TemplateBuilder;
+
+
+class WebhookController extends Controller
+{
+    public function webhook(Request $req){
+        //log events
+        Log::useFiles($this->file_path_line_log);
+        Log::info($req->all());
+        $httpClient = new CurlHTTPClient(config('services.botline.access'));
+        $bot = new LINEBot($httpClient, [
+            'channelSecret' => config('services.botline.secret')
+        ]);
+
+        $signature = $req->header(HTTPHeader::LINE_SIGNATURE);
+        if (empty($signature)) {
+            abort(401);
+        }
+        try {
+            $events = $bot->parseEventRequest($req->getContent(), $signature);
+        } catch (\Exception $e) {
+            logger()->error((string) $e);
+            abort(200);
+        }
+
+        foreach ($events as $event) {
+            switch ($event->getText()) {
+                case 'Menu Comrades' :
+                    $replyMessage = $this->sendFullMenu($event);
+                    break;
+                case 'Cek Artikel Terbaru' :
+                    $replyMessage = $this->sendArtikel();
+                    break;
+                case 'Cek Tweet Komunitas' :
+                    $replyMessage = $this->sendTwitter();
+                    break;
+                case 'Cari Layanan HIV' :
+                    $replyMessage = $this->sendLokasiARV();
+                    break;
+                case 'Tweet Dukungan' :
+                    $replyMessage = $this->sendTweetDukungan();
+                    break;
+                case 'Cek Mitos dan Fakta' :
+                    $replyMessage = $this->sendTweetDukungan();
+                    break;
+                case 'Menu Konsultasi' :
+                    $replyMessage = $this->sendKonsultasi($event);
+                    break;
+                case 'Menu Berita' :
+                    $replyMessage = $this->sendBerita();
+                    break;
+                case 'Menu Artikel' :
+                    $replyMessage = $this->sendArtikel();
+                    break;
+                case 'Menu Lokasi & Layanan ARV' :
+                    $replyMessage = $this->sendLokasiARV();
+                    break;
+                case "Cek Artikel Terbaru":
+                    $replyMessage = $this->sendArtikel();
+                    break;
+                default:
+                    $replyMessage = new TextMessageBuilder('hallo');
+            }
+            $this->simpanMessage(["idUser" => $event->getUserId(),"idMessage"=>$event->getMessageId(), "message" => $event->getText()]);
+            $bot->replyMessage($event->getReplyToken(), $replyMessage);
+        }
+        return response('OK', 200);
+    }
+
+
+
+    
+}
